2b181dc6bdb6cbe3ae8c345e063c1d5faf3b54c4
Michael Myers <michael.myers@goteleport.com>
2024-03-12 14:01:14 -0500

    Use no-cache to revalidate every request

--- lib/httplib/httpheaders.go
+++ lib/httplib/httpheaders.go
@@ -147,7 +147,7 @@ func SetCacheHeaders(h http.Header, maxAge time.Duration) {
 // SetEntityTagCacheHeaders tells proxies and browsers to cache the content
 // and sets an ETag based on teleport version which can be used to check for modifications
 func SetEntityTagCacheHeaders(h http.Header, version string) {
-	h.Set("Cache-Control", "max-age=300")
+	h.Set("Cache-Control", "no-cache")
 	h.Set("ETag", version)
 }
 
